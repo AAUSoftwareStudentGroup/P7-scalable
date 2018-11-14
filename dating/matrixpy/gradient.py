67ef60bad21dcada1868410cb2cb8ce00b4eb397
jgfrausing <jfraus14@student.aau.dk>
2018-11-14 15:13:55 +0100

    Collaborative filtering is working. Tested in other environment.

--- dating/matrixpy/gradient.py
+++ dating/matrixpy/gradient.py
@@ -26,7 +26,7 @@ group_answers= [ [3, 4, 3, 1, 1, 5, 5, 2, 3, 3] # Tessa
 
 test_answers= [  [0, 4, 3, 1, 1, 5, 5, 0, 3, 3] # Tessa
                , [3, 2, 3, 0, 4, 5, 5, 3, 2, 1] # Jonatan
-               , [1, 1, 3, 1, 3, 1, 4, 3, 2, 2] # Jacob
+               , [1, 1, 3, 1, 3, 1, 4, 3, 0, 2] # Jacob
                , [4, 1, 3, 1, 2, 0, 5, 0, 1, 2] # Anton
                , [2, 4, 0, 1, 2, 5, 0, 2, 1, 1] # Henrik
                , [3, 4, 2, 1, 2, 5, 5, 4, 0, 1] # Kasper
@@ -47,7 +47,6 @@ def gradient_descent(A, A_, U, Q):
   guess = np.matmul(U, Q) # 6 * 10
   guess_ = np.multiply(guess, A_) # 6 * 10
   mse = mean_squared_error(A, guess_)
-  print(mse)
   if mse >= 0.59:
     E = guess_ - A # 6 * 10
     #print(f"E:{E}")
@@ -64,7 +63,7 @@ sys.setrecursionlimit(1000000)
 test_answers_ = applyToMatrix(toOneOrZero, test_answers)
 U1, Q1 = gradient_descent(test_answers, test_answers_, U, Q)
 result = np.matmul(U1, Q1)
-print(result - group_answers)
+print(result)
 print(mean_squared_error(result, group_answers))
 
 
