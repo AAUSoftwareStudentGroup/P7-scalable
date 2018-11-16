76ce9ec1ef5332be758e066da431a3777b720422
Henrik <hhs19942@gmail.com>
2018-11-16 14:51:26 +0100

    Scroll to bottom on chat

--- dating/frontend/src/Page/Chat.elm
+++ dating/frontend/src/Page/Chat.elm
@@ -1,101 +1,128 @@
 module Page.Chat exposing (Model, Msg(..), init, subscriptions, update, view)
 
+import Api.Messages exposing (Conversation, Message)
+import Api.Users
+import Browser.Dom as Dom exposing (Viewport)
 import Html exposing (Html, div)
 import Html.Attributes as Attributes exposing (classList)
 import Html.Events as Events
 import Html.Keyed exposing (ul)
 import Http
-import String as String
-import Task as Task
-import Time as Time
-
-import Api.Messages exposing (Message, Conversation)
-import Api.Users
-
 import Routing exposing (Route(..))
-import Session as Session exposing (Session, Details)
+import Session as Session exposing (Details, Session)
+import String
+import Task
+import Time
 import UI.Elements as El
+
+
 
 -- MODEL
 
-startPage = 0
-messagesPerPage = 100
+
+startPage =
+    0
+
+
+messagesPerPage =
+    100
+
+
+listId =
+    "message-list"
+
 
 type alias Model =
-    { session           : Session
-    , title             : String
-    , loaded            : Bool
-    , content           : List Message
-    , usernameFriend    : String
-    , usernameSelf      : String
-    , numMsgs           : Int
-    , unsentMessage     : String
-    , zone              : Time.Zone
-    , time              : Time.Posix
+    { session : Session
+    , title : String
+    , loaded : Bool
+    , messages : List Message
+    , numMessages : Int
+    , usernameSelf : String
+    , usernameFriend : String
+    , unsentMessage : String
     }
 
 
 emptyModel : Session -> Model
 emptyModel session =
-    Model session "Messages" False [] "" "" 0 "" Time.utc (Time.millisToPosix 0)
+    Model session "Messages" False [] 0 "" "" ""
 
 
 init : Session -> String -> ( Model, Cmd Msg )
 init session usernameFriend =
-    case session of
-        Session.Guest _ _ ->
+    let
+        redirect =
             ( emptyModel session
             , Routing.goHome (Session.getNavKey session)
             )
-        Session.LoggedIn _ _ userInfo ->
-            ( Model session "Messages" False [] usernameFriend userInfo.username 0 "" Time.utc (Time.millisToPosix 0)
-            , getMessagesOrRedirect session userInfo.username usernameFriend
-            )
+    in
+    case session of
+        Session.Guest _ _ ->
+            redirect
 
-getMessagesOrRedirect : Session -> String -> String -> Cmd Msg
-getMessagesOrRedirect session usernameSelf usernameFriend =
-    if usernameSelf == usernameFriend then
-        Routing.replaceUrl (Session.getNavKey session) (Routing.routeToString Home)
-    else
-        Cmd.batch
-            [ Task.perform AdjustTimeZone Time.here
-            , Task.perform FetchMessages Time.now
-            ]
+        Session.LoggedIn _ _ userInfo ->
+            if userInfo.username == usernameFriend then
+                redirect
+
+            else
+                ( Model session "Messages" False [] 0 userInfo.username usernameFriend ""
+                , Task.perform FetchMessages Time.now
+                )
+
+
 
 -- UPDATE
 
+
 type Msg
-    = AdjustTimeZone Time.Zone
+    = NoOp
     | FetchMessages Time.Posix
     | HandleFetchedMessages (Result Http.Error Conversation)
     | UnsentMessageChanged String
     | SendMessage
-    | HandleMessageSent (Result Http.Error (String.String))
+    | HandleMessageSent (Result Http.Error String.String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        AdjustTimeZone newZone ->
-            ( { model | zone = newZone }, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
 
         FetchMessages newTime ->
-            case (model.session) of
-                Session.Guest _ _->
-                    ( { model | time = newTime }, Cmd.none)
+            case model.session of
+                Session.Guest _ _ ->
+                    ( model, Cmd.none )
+
                 Session.LoggedIn _ _ userInfo ->
-                    ( { model | time = newTime }
+                    ( model
                     , Http.send HandleFetchedMessages
-                        (Api.Messages.getMessagesFromUsername userInfo model.usernameFriend model.numMsgs messagesPerPage)
+                        (Api.Messages.getMessagesFromUsername userInfo model.usernameFriend model.numMessages messagesPerPage)
                     )
 
         HandleFetchedMessages result ->
             case result of
                 Ok conversation ->
-                    ( { model | content = List.append model.content conversation.messages
-                      , loaded = True
-                      , numMsgs = model.numMsgs + List.length conversation.messages
-                      }, Cmd.none)
+                    let
+                        numNewMessages =
+                            List.length conversation.messages
+
+                        command =
+                            if numNewMessages == 0 then
+                                Cmd.none
+
+                            else
+                                jumpToBottom listId
+                    in
+                    ( { model
+                        | messages = List.append model.messages conversation.messages
+                        , numMessages = model.numMessages + numNewMessages
+                        , loaded = True
+                      }
+                    , command
+                    )
+
                 Err _ ->
                     ( model, Cmd.none )
 
@@ -109,8 +136,9 @@ update msg model =
             case result of
                 Ok responseString ->
                     ( { model | unsentMessage = "" }, Task.perform FetchMessages Time.now )
+
                 Err _ ->
-                    ( model , Cmd.none  )
+                    ( model, Cmd.none )
 
 
 
@@ -121,6 +149,8 @@ subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch [ Time.every 3000 FetchMessages ]
 
+
+
 -- VIEW
 
 
@@ -129,21 +159,23 @@ view model =
     { title = model.title
     , session = model.session
     , kids =
-        El.titledContentLoader model.loaded ("Chatting with " ++ model.usernameFriend)
+        El.titledContentLoader model.loaded
+            ("Chatting with " ++ model.usernameFriend)
             [ ul
                 [ classList
                     [ ( "messages", True )
                     , ( "l-12", True )
                     , ( "l-6", True )
                     ]
+                , Attributes.id listId
                 ]
-                (List.map (viewMessage model) model.content)
+                (List.map (viewMessage model) model.messages)
             , Html.form
                 [ Events.onSubmit SendMessage
                 , classList
-                      [ ( "l-12", True )
-                      , ( "l-6", True )
-                      ]
+                    [ ( "l-12", True )
+                    , ( "l-6", True )
+                    ]
                 ]
                 [ El.simpleInput "text" "message" model.unsentMessage UnsentMessageChanged False
                 , El.submitButtonHtml
@@ -152,36 +184,45 @@ view model =
             ]
     }
 
-viewMessage : Model -> Message -> (String, Html Msg)
+
+viewMessage : Model -> Message -> ( String, Html Msg )
 viewMessage model message =
     let
-        myMessage = model.usernameSelf == message.authorName
+        myMessage =
+            model.usernameSelf == message.authorName
     in
-        ( message.timeStamp
-        , Html.li
+    ( message.timeStamp
+    , Html.li
+        [ classList
+            [ ( "message", True ) ]
+        ]
+        [ div
             [ classList
-                [ ( "message", True ) ]
-            ]
-            [ div
-                [ classList
-                    [ ( "author-me", myMessage)
-                    , ( "author-friend", not myMessage)
-                    ]
+                [ ( "author-me", myMessage )
+                , ( "author-friend", not myMessage )
                 ]
-                [ Html.text message.body ]
             ]
-        )
+            [ Html.text message.body ]
+        ]
+    )
 
 
 sendMessage : Model -> Cmd Msg
 sendMessage model =
-    if (String.isEmpty model.unsentMessage) then
+    if String.isEmpty model.unsentMessage then
         Cmd.none
+
     else
         case model.session of
             Session.LoggedIn _ _ userInfo ->
                 Http.send HandleMessageSent (Api.Messages.postMessage userInfo model.unsentMessage model.usernameFriend)
+
             Session.Guest _ _ ->
                 Cmd.none
 
 
+jumpToBottom : String -> Cmd Msg
+jumpToBottom id =
+    Dom.getViewportOf id
+        |> Task.andThen (\info -> Dom.setViewportOf id 0 info.scene.height)
+        |> Task.attempt (\_ -> NoOp)
