b85112c3244134075f7e404c4c6b02d34ae36b4c
Anton Christensen <anton.christensen9700@gmail.com>
2018-11-19 09:14:46 +0100

    Adds survey data and extraction script

---
+++ surveyAnswers/importSurveyData.py
@@ -0,0 +1,58 @@
+#!/bin/env python3
+# {
+#     "_id" : ObjectId("5bed30cb5d8bd881161c57b4"),
+#     "text" : "test text",
+#     "user_answers" : [ 
+#     ],
+#     "survey_answers" : [ 
+#         {
+#             "u_id" : 1234,
+#             "score" : 1
+#         }, 
+#         {
+#             "u_id" : 1234,
+#             "score" : 1
+#         }, 
+#         {
+#             "u_id" : 1234,
+#             "score" : 1
+#         }
+#     ]
+# }
+
+import json
+def readfile(file):
+	with open(file) as f:
+		return f.readlines()
+
+userid = 0
+questions = {}
+
+for i in range(1,2):
+	quest_text = readfile(f"questions/paired-survey-{i}-codebook.txt")
+	user_answers = readfile(f"answers/paired-survey-{i}-responses.csv")
+	for answer in user_answers[1:]:
+		answer = answer.split("\t")[4:]
+
+		for j in range(0, len(quest_text)):
+			if(j == 25):
+				break
+			
+			txt = quest_text[j].split("\t")[1].strip()
+			if(txt not in questions):
+				questions[txt] = {}
+			questions[txt][userid] = int(answer[j])
+
+		userid += 1
+
+dump = []
+
+for question_text, user_answer_dict in questions.items():
+	document = {'text': question_text, 'survey_answers': []}
+	for userid, score in user_answer_dict.items():
+		document['survey_answers'].append({'respondent_id': userid, 'score': score})
+		
+	dump.append(json.dumps(document))
+
+with open("survey_answers.json", '+w') as f:
+	f.write('['+',\n'.join(dump)+']') 
